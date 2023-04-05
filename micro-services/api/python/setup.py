f8bdd69de16674f6c657921f5c769d6c7674041b
karora-wolferesearch <karora@wolferesearch.com>
2023-04-05 21:11:28 +0000

    Added setup file for pyqes to make it pip installable

---
+++ micro-services/api/python/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'pyqes',
+      version = '0.1',
+      description = 'Wrapper for QLaaS RESTful API',
+      author = 'Kartik Arora',
+      author_email = 'karora@wolferesearch.com',
+      license = 'Proprietary',
+      packages = ['pyqes'],
+      zip_safe = False, requires=['requests','time','os', 'tempfile','datetime','pandas','json','io','urllib'])
