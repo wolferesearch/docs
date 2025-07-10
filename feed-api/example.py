c04c9cba0caa98b522dbcf3dad70f85ca5ba89f8
karora-wolferesearch <karora@wolferesearch.com>
2025-07-10 22:44:40 +0000

    Updated the Product API

--- feed-api/example.py
+++ feed-api/example.py
@@ -6,11 +6,11 @@ client = QESClient("qes_url","your_username", "your_password")
 products = client.get_products()
 
 # Get details for a specific product
-details = client.get_product_details(196)
+details = client.get_product_details(197)
 
 # Get time series data for a security
-ts_data = client.get_security_data("AAPL US", "TRAP", "2023-01-01", "2023-12-31")
+ts_data = client.get_security_data(197,"TRAP","TIC","AAPL", "2023-01-01", "2023-12-31")
 
 # Get cross-sectional data for a product on a specific date
-cs_data = client.get_cross_sectional_data("TRAP", "2023-12-31")
+cs_data = client.get_cross_sectional_data(197,"2024-06-20")
 
--- feed-api/qes_client.py
+++ feed-api/qes_client.py
@@ -1,6 +1,60 @@
 
 import requests
 from urllib.parse import urljoin
+import pandas as pd
+import numpy as np
+
+import urllib3
+urllib3.disable_warnings()
+
+class QesData:
+    def __init__(self, data):
+        self.data = data
+        self.empty = len(data) == 0
+        self.n_rows = len(data.get('$rows', []))
+        self.df_data = {}
+
+    def rows(self):
+        return self.data.get('$rows')
+
+    def _extract_section(self, section: dict):
+        cols = section.get('$cols', [])
+        vals = section.get('$vals', [])
+        if not cols or not vals:
+            return
+        reshaped = np.array(vals).reshape(self.n_rows,len(cols))
+        for i, col in enumerate(cols):
+            self.df_data[col] = reshaped[:, i]
+
+    def dates(self):
+        return pd.to_datetime(self.rows())
+        
+    def as_time_series(self):
+        if self.empty:
+            return None
+
+        num = self.data.get('$num')
+        if num is None:
+            return None
+        
+        # Create Series
+        return pd.Series(data = num.get('$vals',[]), index = self.dates(), name = num.get('$cols')[0])
+        
+    def as_data_frame(self) -> pd.DataFrame:
+        # Process numerical columns
+        if '$num' in self.data:
+            self._extract_section(self.data['$num'])
+
+        # Process string columns
+        if '$str' in self.data:
+            self._extract_section(self.data['$str'])
+
+        # Add date column
+        df = pd.DataFrame(self.df_data)
+        if '$rows' in self.data:
+            df.insert(0, 'Id', self.rows())
+        
+        return df
 
 class APIClient:
     def __init__(self, url, username, password):
@@ -26,12 +80,13 @@ class QESClient(APIClient):
         path = f"/qes/product/{productid}"
         return self.request("GET", path)
 
-    def get_security_data(self, idtype: str, 
-                          securityid: str, 
-                          productid: int, 
-                          item: str, startdate: str, enddate: str):
+    def get_security_data(self, productid: int,
+                          item: str,
+                          idtype: str, 
+                          securityid: str,  
+                          startdate: str, enddate: str):
         """
-        GET /qes/data/ts/{securityid}/{item}/{startdate}/{enddate}
+        GET /qes/product/{productid}/data/ts/{idtype}/{securityid}/{item}/{startdate}/{enddate}
         Get time series data for a security
 
         - idtype: Type of ID (e.g., 'QESID', 'CUSIP', 'SEDOL','TIC')
@@ -43,8 +98,9 @@ class QESClient(APIClient):
         - Returns: JSON object with time series data for the security
         """
         path = f"/qes/product/{productid}/data/ts/{idtype}/{securityid}/{item}/{startdate}/{enddate}"
-        return self.request("GET", path)
+        return QesData(self.request("GET", path)).as_time_series()
 
+        
     def get_cross_sectional_data(self, productid: int, dated: str):
         """
         GET /qes/data/cs/{productid}/{dated}
@@ -54,7 +110,7 @@ class QESClient(APIClient):
             - Returns: JSON object with cross-sectional data    
         """
         path = f"/qes/product/{productid}/data/cs/{dated}"
-        return self.request("GET", path)
+        return QesData(self.request("GET", path))
     
     def get_security_info(self, qesid: str):
         """
